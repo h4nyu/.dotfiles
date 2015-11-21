dff0f08355a603d6de1dc5e44e0e322a6d093d54
x1nyuan <yao.ntno@gmail.com>
2015-11-21 16:04:03 +0900

    monaco

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+curl -kL https://raw.github.com/cstrap/monaco-font/master/install-font-ubuntu.sh | bash
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 rm -rf ~/.config/xfce4
