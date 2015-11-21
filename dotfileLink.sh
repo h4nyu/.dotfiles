50defbbc5a63052b4601d337ac19e546e0c494e6
x1nyuan <yao.ntno@gmail.com>
2015-11-21 16:19:34 +0900

    Thunar

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -5,3 +5,5 @@ ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 rm -rf ~/.config/xfce4
 ln -sf ~/.dotfiles/config/xfce4 ~/.config/xfce4
+rm -rf ~/.config/Thunar
+ln -sf ~/.dotfiles/config/Thunar ~/.config/Thunar
