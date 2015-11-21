1c1c97a722625306c883b6121ecb7ee3a32cd484
x1nyuan <yao.ntno@gmail.com>
2015-11-21 16:55:50 +0900

    aaaa

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo apt-get install curl
-curl -kL https://raw.github.com/cstrap/monaco-font/master/install-font-ubuntu.sh | bash
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 rm -rf ~/.config/xfce4
---
+++ set_up.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo apt-get install curl
+curl -kL https://raw.github.com/cstrap/monaco-font/master/install-font-ubuntu.sh | bash
