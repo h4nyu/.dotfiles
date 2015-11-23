26809c4d7c63de57837738bda072a13621ac8ce7
x1nyuan <yao.ntno@gmail.com>
2015-11-23 10:58:42 +0900

    aaa

--- set_up.sh
+++ set_up.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo apt-get -y install curl aptitude vim-gtk nautilus-dropbox
+sudo apt-get -y install curl aptitude vim-gtk nautilus-dropbox ibus-mozc
 curl -kL https://raw.github.com/cstrap/monaco-font/master/install-font-ubuntu.sh | bash
