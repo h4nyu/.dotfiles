09a5b05dfbcebb482413e08bf1c1680e52a02131
x1nyuan <yao.ntno@gmail.com>
2016-01-17 17:30:38 +0900

    AAA

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -3,9 +3,8 @@ git config --global user.name "x1nyuan"
 git config --global user.email "yao.ntno@gmail.com"
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/.vnc/xstartup ~/.vnc/xstartup
 rm -rf ~/.config/xfce4
 ln -sf ~/.dotfiles/config/xfce4 ~/.config/xfce4
 rm -rf ~/.config/Thunar
 ln -sf ~/.dotfiles/config/Thunar ~/.config/Thunar
-rm -rf ~/.vnc
-ln -sf ~/.dotfiles/.vnc
