e35bc80b82eeeecddb758e50dba5089db30e04c1
x1nyuan <yao.ntno@gmail.com>
2016-01-17 17:58:15 +0900

    aaaa

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -3,7 +3,15 @@ git config --global user.name "x1nyuan"
 git config --global user.email "yao.ntno@gmail.com"
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
-mkdir ~/.vnc
+
+DIR=~/.vnc
+
+if [ -d ${DIR} ]; then
+    echo "folder exists"
+else
+    mkdir ~/.vnc
+fi
+
 ln -sf ~/.dotfiles/.vnc/xstartup ~/.vnc/xstartup
 rm -rf ~/.config/xfce4
 ln -sf ~/.dotfiles/config/xfce4 ~/.config/xfce4
