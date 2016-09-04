cb5259c938f4884f6d1f9e250d6e45040f60ce8a
x1nyuan <yao.ntno@gmail.com>
2016-09-04 15:59:15 +0900

    rename

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -4,18 +4,18 @@ sh ./exit_root.sh
 
 git config --global user.name "x1nyuan"
 git config --global user.email "yao.ntno@gmail.com"
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc
-ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/bashrc ~/.bashrc
 
 rm -rf ~/.pandoc
-ln -sf ~/.dotfiles/.pandoc ~/.pandoc
+ln -sf ~/.dotfiles/pandoc ~/.pandoc
 
 rm -rf ~/.config/lxterminal
 if [ ! -e ~/.config ]; then
     echo "no dir"
     mkdir ~/.config
 fi
-ln -sf ~/.dotfiles/.config/lxterminal ~/.config/lxterminal
+ln -sf ~/.dotfiles/config/lxterminal ~/.config/lxterminal
 
 rm -rf ~/.config/lxsession
-ln -sf ~/.dotfiles/.config/lxsession ~/.config/lxsession
+ln -sf ~/.dotfiles/config/lxsession ~/.config/lxsession
