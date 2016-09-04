e1bb5aacdefa49cb5aa53b38cbbb59983c284661
x1nyuan <yao.ntno@gmail.com>
2016-09-04 04:27:58 +0000

    add skype install

--- set_up.sh
+++ set_up.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo add-apt-repository "deb http://archive.canonical.com/$(lsb_release -sc) partner"
+
 sudo apt-get update 
 sudo apt-get -y upgrade
 sudo apt-get -y install curl 
@@ -9,15 +11,10 @@ sudo apt-get -y install ibus-mozc
 sudo apt-get -y install pandoc
 sudo apt-get -y install python-pip 
 sudo apt-get -y install pepperflashplugin-nonfree
-#sudo add-apt-repository ppa:mystic-mirage/pycharm
-#sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get install skype
 
 # monaco font for programing
 sudo cp -r monaco /usr/share/fonts/truetype
-
-# anaconda python
-# wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
-
 
 sh ./dotfileLink.sh
 echo setup finished
