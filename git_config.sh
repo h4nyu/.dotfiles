b981e6a3aafe93e0688852343c8b9456a7e1f9db
x1nyuan <yao.ntno@gmail.com>
2017-02-23 21:51:25 +0900

    add git config

---
+++ git_config.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sh ./exit_root.sh
+
+git config --global user.name "x1nyuan"
+git config --global user.email "yao.ntno@gmail.com"
+git config --global push.default simple
+
+git config --global merge.tool vimdiff
+git config --global merge.conflictstyle diff3
+git config --global mergetool.prompt false
