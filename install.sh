17694f41c63f1a894a4ee85800cc124b65937466
x1nyuan <yao.ntno@gmail.com>
2018-02-20 01:44:18 +0900

    remove unuse files

--- install.sh
+++ install.sh
@@ -1,27 +1,18 @@
-#!/bin/sh
-sudo add-apt-repository ppa:graphics-drivers/ppa
+#!/bin/bash
 
 sudo -E apt-get update 
-sudo -E apt-get -y dist-upgrade 
 sudo -E apt-get -y upgrade 
 
 suda -E apt-get autoremove
 sudo -E apt-get -y install curl 
 sudo -E apt-get -y install tmux 
-sudo -E apt-get -y install aptitude
 sudo -E apt-get -y install vim-nox
-sudo -E apt-get -y install fcitx-mozc 
-sudo -E apt-get -y install pandoc
-sudo -E apt-get -y install python-pip 
-sudo -E apt-get -y install python3-pip 
-sudo -E apt-get -y install flashplugin-installer 
-sudo -E apt-get -y install arandr
-sudo -E apt-get -y install ctags
-sudo -E apt-get -y install clang
-sudo -E apt-get -y install python-wstool python-catkin-tools
 
+sudo cp -r monaco /usr/share/fonts/truetype
 
-# python packege
-sudo -E -H pip install flake8 autopep8 jedi
+./exit_root.sh
+
+cp ~/.dotfiles/tmux.conf ~/.tmux.conf
+cp ~/.dotfiles/bashrc ~/.bashrc
 
 exit
