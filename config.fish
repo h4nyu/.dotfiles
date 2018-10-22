5365804b97aff777f570ef89ce67e72d21413dbe
higuchi.fumito <higuchi.fumito@canon.co.jp>
2018-10-22 18:37:52 +0900

    fix ag

--- config.fish
+++ config.fish
@@ -7,12 +7,13 @@ set -x LSCOLORS exfxcxdxbxegedabagacad
 set -x LS_COLORS 'di=01;94:ln=01;35:so=01;32:ex=01;31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 set -x EDITOR vim
 set -x TERM xterm-256color
+set -x FZF_DEFAULT_COMMAND 'ag --hidden -g ""'
 
 
 alias gs='git status' 
 alias gp='git push' 
 alias gf='git fetch' 
-alias ag='ag -S -p ~/.ignore' 
+
 
 git config --global push.default simple
 git config --global merge.tool vimdiff
