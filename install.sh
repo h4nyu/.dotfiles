78ee029cb45e8a28047526e84c6425a5fc756d4e
xinyuan yao <yao.ntno@gmail.com>
2019-02-02 10:50:09 +0000

    fix install

--- install.sh
+++ install.sh
@@ -6,13 +6,14 @@ ln -sf ~/.dotfiles/config.fish ~/.config/fish/config.fish
 ln -sf ~/.dotfiles/ignore ~/.ignore
 ln -sf ~/.dotfiles/vim/.vimrc ~/.vimrc
 
+
 if [ ! -f ~/.vim ]; then
-    mkdir ~/.viM
-    ln -sf ~/.dotfiles/vim/src ~/.vim
+    mkdir -p ~/.vim/src
+    cp ~/.dotfiles/vim/src/* ~/.vim/src
 fi
 
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 if [ ! -d ~/.fzf ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
