1403ea691c30f1439e80a62449d757bf87ec9c95
h4nyu <yao.ntno@gmail.com>
2021-11-11 11:39:14 +0900

    add docker-compose bash_completion

--- install.sh
+++ install.sh
@@ -19,3 +19,9 @@ if [ ! -d ~/.fzf ]; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install
 fi
+
+
+if [ ! -d ~/.bash_completion ]; then
+    mkdir -p ~/.bash_completion
+fi
+curl -L https://raw.githubusercontent.com/docker/compose/1.29.2/contrib/completion/bash/docker-compose -o ~/.bash_completion/docker-compose
