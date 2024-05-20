5a1e7db87eb3862c80f94bac2a85b4068b6f99a8
XinyuanYao <yao.ntno@gmail.com>
2024-05-20 23:16:17 +0000

    disable font

--- init.lua
+++ init.lua
@@ -140,9 +140,14 @@ require("lazy").setup({
   },
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function() 
-      require('lualine').setup()
+      require('lualine').setup({
+        options = {
+          icons_enabled = false,
+          component_separators = { left = '|', right = '|'},
+          section_separators = { left = '', right = ''},
+        },
+      })
     end
   },
   {
