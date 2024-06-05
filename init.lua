629bbd168c4ab767537f2251d60863fe555a1157
XinyuanYao <yao.ntno@gmail.com>
2024-06-05 22:13:01 +0000

    fix luialine

--- init.lua
+++ init.lua
@@ -148,6 +148,15 @@ require("lazy").setup({
     'nvim-lualine/lualine.nvim',
     config = function() 
       require('lualine').setup({
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
+            }
+          }
+        },
         options = {
           icons_enabled = false,
           component_separators = { left = '|', right = '|'},
