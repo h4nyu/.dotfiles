c1180f8764462ef13ce5d6644cdf9b4ed94f2c21
h4nyu <yao.ntno@gmail.com>
2025-09-01 11:17:13 +0900

    update comment

--- init.lua
+++ init.lua
@@ -190,11 +190,20 @@ require("lazy").setup({
     end
   },
   {
-    'tpope/vim-commentary',
-    config = function() 
-      vim.api.nvim_set_keymap("x", "<Leader>c", ":Commentary<CR>", { noremap = true, silent = true })
-      vim.api.nvim_set_keymap("n", "<Leader>c", ":Commentary<CR>", { noremap = true, silent = true })
-    end
+    'numToStr/Comment.nvim',
+    opts = {
+      toggler = {
+        line = '<leader>c',
+      },
+      opleader = {
+        ---Line-comment keymap
+        line = '<leader>c',
+      },
+      mappings = {
+        basic = true,
+        extra = false,
+      },
+    }
   },
   {
     "peitalin/vim-jsx-typescript"
