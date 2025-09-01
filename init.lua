dff50ce6ec2005e2acb9270d8cf266408656aa13
h4nyu <yao.ntno@gmail.com>
2025-09-01 13:04:04 +0900

    update file path

--- init.lua
+++ init.lua
@@ -167,8 +167,7 @@ require("lazy").setup({
           lualine_c = {
             {
               'filename',
-              file_status = true, -- displays file status (readonly status, modified status)
-              path = 2 -- 0 = just filename, 1 = relative path, 2 = absolute path
+              path = 1
             }
           }
         },
